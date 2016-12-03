190b49bea820f19df40055853c49bbd05ce0c048
mren2006 <mren2006@gmail.com>
2016-12-02 23:41:01 -0500

    Start the project solution framework with CICD type of DevOps link to pulbic cloud web site so that end users/Stakeholders can see the progress immediately

--- Fibonacci/src/Fibonacci/Controllers/HomeController.cs
+++ Fibonacci/src/Fibonacci/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Fibonacci.Models;
 
 namespace Fibonacci.Controllers
 {
@@ -13,6 +14,7 @@ namespace Fibonacci.Controllers
             return View();
         }
 
+
         public IActionResult Calculate()
         {
             ViewData["Message"] = "Calculate Fibonacci Here.";
@@ -20,13 +22,49 @@ namespace Fibonacci.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
-            return View();
+
+        public IActionResult Result(Fibo F)
+        {
+            int n = F.num;
+
+            F.Iteration = FiboIteration(n);
+
+            F.Recursion = FiboRecursion(n);
+
+            return View(F);
+
         }
 
+        int FiboIteration(int n)
+        {
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            int prev2 = 0;
+            int prev1 = 1;
+            int res = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                res = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = res;
+            }
+
+            return res;
+        }
+        
+
+        int FiboRecursion(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return FiboRecursion(n - 1) + FiboRecursion(n - 2);
+        }
+        
         public IActionResult Error()
         {
             return View();
---
+++ Fibonacci/src/Fibonacci/Models/Fibo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fibonacci.Models
+{
+    public class Fibo
+    {
+        // GET: /Calculations/
+        public int num { get; set; }
+        public int Recursion { get; set; }
+        public int Iteration { get; set; }
+
+
+    }
+}
